b6d160e906f3962b2d9509c48d5cedfedd9c50bd
voldemortX <zyfeng97@gmail.com>
2020-07-06 16:19:12 +0800

    Everything done, except training & testing

--- code/list_convertor.py
+++ code/list_convertor.py
@@ -5,13 +5,16 @@
 
 import os
 root = '../../../dataset/tusimple/lists'
-file_names = ['train.txt', 'val.txt', 'trainval.txt']
+file_names = ['train.txt', 'val.txt', 'trainval.txt', 'test.txt']
 for file_name in file_names:
     file_name = os.path.join(root, file_name)
     with open(file_name, 'r') as f:
         temp = f.readlines()
     for x in range(len(temp)):
-        temp[x] = temp[x][temp[x].find('clips/') + 6: temp[x].find('.jpg')] + temp[x][temp[x].find('.png') + 4:]
+        if 'test.txt' in file_name:
+            temp[x] = temp[x][temp[x].find('clips/') + 6: temp[x].find('.jpg')] + '\n'
+        else:
+            temp[x] = temp[x][temp[x].find('clips/') + 6: temp[x].find('.jpg')] + temp[x][temp[x].find('.png') + 4:]
     with open(file_name, 'w') as f:
         f.writelines(temp)
 
