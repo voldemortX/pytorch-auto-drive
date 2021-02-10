433889053e5d90cd99426c86613dceaf46ce50cf
voldemortX <zyfeng97@gmail.com>
2021-02-10 20:54:52 +0800

    Add initial LSTR model and transformers

--- torchvision_models/resnet.py
+++ torchvision_models/resnet.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from .utils import load_state_dict_from_url
 
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+__all__ = ['ResNet', 'resnet18', 'resnet18_reduced', 'resnet34', 'resnet50', 'resnet101',
            'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
            'wide_resnet50_2', 'wide_resnet101_2']
 
