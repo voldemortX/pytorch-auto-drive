045597296ce5c89fc7763392d826dca61cf7419a
Zhengyang Feng <zyfeng97@outlook.com>
2021-03-26 16:09:51 +0800

    FPS benchmark (#9)
    * add lane speed test
    
    * add simlpe test
    
    * bug
    
    * reformat
    
    * remove count
    
    * The segmentation speed test model is under programming
    
    * segmentation
    
    * fps test
    
    * rename
    
    Co-authored-by: cedricgsh@sina.com <cedricgsh@sina.com>

---
+++ profiling.py
@@ -0,0 +1,129 @@
+import yaml
+import argparse
+
+from utils.all_utils_landec import build_lane_detection_model as build_lane_model
+from utils.all_utils_semseg import build_segmentation_model
+from utils.all_utils_semseg import load_checkpoint
+from tools.profiling_utils import init_lane, init_seg
+from tools.profiling_utils import speed_evaluate_real, speed_evaluate_simple
+import torch
+
+if __name__ == '__main__':
+    # Settings
+    parser = argparse.ArgumentParser(description='PyTorch Auto-drive')
+    parser.add_argument('--height', type=int, default=288,
+                        help='Image input height (default: 288)')
+    parser.add_argument('--width', type=int, default=800,
+                        help='Image input width (default: 800)')
+    parser.add_argument('--dataset', type=str, default='tusimple',
+                        help='Profile on TuSimple (tusimple) / CULane (culane) (default: tusimple)')
+    parser.add_argument('--method', type=str, default='baseline',
+                        help='method selection (lstr/scnn/sad/baseline) (default: baseline)')
+    parser.add_argument('--backbone', type=str, default='erfnet',
+                        help='backbone selection (erfnet/enet/vgg16/resnet18s/resnet18/resnet34/resnet50/resnet101)'
+                             '(default: erfnet)')
+    parser.add_argument('--mixed-precision', action='store_true', default=False,
+                        help='Enable mixed precision training (default: False)')
+    parser.add_argument('--task', type=str, default='lane',
+                        help='task selection (lane/seg)')
+    parser.add_argument('--mode', type=str, default='simple',
+                        help='Profiling mode (simple/real)')
+    parser.add_argument('--model', type=str, default='deeplabv3',
+                        help='Model selection (fcn/erfnet/deeplabv2/deeplabv3/enet) (default: deeplabv3)')
+    parser.add_argument('--inf-times', type=int, default=1,
+                        help='Select test times')
+    parser.add_argument('--encoder-only', action='store_true', default=False,
+                        help='Only train the encoder. ENet trains encoder and decoder separately (default: False)')
+    parser.add_argument('--state', type=int, default=1,
+                        help='train the whole enet(2)/Conduct final test(1)/normal training(0) (default: 0)')
+    parser.add_argument('--continue-from', type=str, default=None,
+                        help='Continue training from a previous checkpoint')
+    args = parser.parse_args()
+
+    lane_need_interpolate = ['vgg16', 'resnet18s', 'resnet18', 'resnet34', 'resnet50', 'resnet101']
+    seg_need_interpolate = ['fcn', 'deeplabv2', 'deeplabv3']
+    with open('configs.yaml', 'r') as f:  # Safer and cleaner than box/EasyDict
+        configs = yaml.load(f, Loader=yaml.Loader)
+
+    mean = configs['GENERAL']['MEAN']
+    std = configs['GENERAL']['STD']
+
+    if args.task == 'lane':
+        num_classes = configs[configs['LANE_DATASETS'][args.dataset]]['NUM_CLASSES']
+        count_interpolate = False
+        if args.backbone in lane_need_interpolate:
+            count_interpolate = True
+        if torch.cuda.is_available():
+            device = torch.device('cuda:0')
+        net = build_lane_model(args, num_classes)
+        print(device)
+        net.to(device)
+
+        print('Profiling, please clear your GPU memory before doing this.')
+        if args.mode == 'simple':
+            dummy = torch.ones((1, 3, args.height, args.width))
+            fps = []
+            for i in range(0, args.inf_times):
+                fps.append(speed_evaluate_simple(net=net, device=device, dummy=dummy, num=300, count_interpolate=True))
+            print('GPU FPS: {: .2f}'.format(max(fps)))
+        elif args.mode == 'real' and args.dataset in configs['LANE_DATASETS'].keys():
+
+            load_checkpoint(net=net, optimizer=None, lr_scheduler=None, filename=args.continue_from)
+            base = configs[configs['LANE_DATASETS'][args.dataset]]['BASE_DIR']
+            val_loader = init_lane(dataset=args.dataset, input_sizes=(args.height, args.width), mean=mean, std=std,
+                                   base=base)
+            fps = []
+            gpu_fps = []
+            for i in range(0, args.inf_times):
+                fps_item, gpu_fps_item = speed_evaluate_real(net=net, device=device, loader=val_loader, num=300,
+                                                             count_interpolate=True)
+                fps.append(fps_item)
+                gpu_fps.append(gpu_fps_item)
+            print('Real FPS: {: .2f}'.format(max(fps)))
+            print('GPU FPS: {: .2f}'.format(max(gpu_fps)))
+        else:
+            raise ValueError
+
+    elif args.task == 'seg':
+        num_classes = configs[configs['SEGMENTATION_DATASETS'][args.dataset]]['NUM_CLASSES']
+        input_sizes = (args.height, args.width)
+        city_aug = 0
+        count_interpolate = False
+        if args.backbone in seg_need_interpolate:
+            count_interpolate = True
+
+        if torch.cuda.is_available():
+            device = torch.device('cuda:0')
+        print(device)
+
+        net, city_aug, _, _ = build_segmentation_model(configs, args, num_classes, city_aug, input_sizes)
+        net.to(device)
+
+        print('Profiling, please clear your GPU memory before doing this.')
+        if args.mode == 'simple':
+            dummy = torch.ones((1, 3, args.height, args.width))
+            fps = []
+            for i in range(0, args.inf_times):
+                fps.append(speed_evaluate_simple(net=net, device=device, dummy=dummy, num=300, count_interpolate=True))
+            print('GPU FPS: {: .2f}'.format(max(fps)))
+        elif args.mode == 'real' and args.dataset in configs['SEGMENTATION_DATASETS'].keys():
+            load_checkpoint(net=net, optimizer=None, lr_scheduler=None, filename=args.continue_from)
+
+            base = configs[configs['SEGMENTATION_DATASETS'][args.dataset]]['BASE_DIR']
+            train_label_id_map = configs[configs['SEGMENTATION_DATASETS'][args.dataset]]['LABEL_ID_MAP'] if \
+                'LABEL_ID_MAP' in configs[configs['SEGMENTATION_DATASETS'][args.dataset]].keys() else \
+                configs['CITYSCAPES']['LABEL_ID_MAP']
+
+            val_loader = init_seg(dataset=args.dataset, input_sizes=(args.height, args.width), mean=mean,
+                                  std=std, test_base=base, city_aug=city_aug, test_label_id_map=train_label_id_map)
+            fps = []
+            gpu_fps = []
+            for i in range(0, args.inf_times):
+                fps_item, gpu_fps_item = speed_evaluate_real(net=net, device=device, loader=val_loader, num=300,
+                                                             count_interpolate=count_interpolate)
+                fps.append(fps_item)
+                gpu_fps.append(gpu_fps_item)
+            print('Real FPS: {: .2f}'.format(max(fps)))
+            print('GPU FPS: {: .2f}'.format(max(gpu_fps)))
+        else:
+            raise ValueError
---
+++ tools/profiling_utils.py
@@ -0,0 +1,113 @@
+import torch
+from tqdm import tqdm
+from utils.datasets import StandardLaneDetectionDataset
+from transforms import ToTensor, Normalize, Resize, Compose, ZeroPad, LabelMap
+from utils.datasets import StandardSegmentationDataset
+import time
+
+
+def init_lane(input_sizes, dataset, mean, std, base, workers=0):
+    transforms_test = Compose(
+        [Resize(size_image=input_sizes, size_label=input_sizes),
+         ToTensor(),
+         Normalize(mean=mean, std=std)])
+
+    validation_set = StandardLaneDetectionDataset(root=base, image_set='val', transforms=transforms_test,
+                                                  data_set=dataset)
+    validation_loader = torch.utils.data.DataLoader(dataset=validation_set, batch_size=1, num_workers=workers,
+                                                    shuffle=False)
+    return validation_loader
+
+
+def init_seg(input_sizes, std, mean, dataset, test_base=None, test_label_id_map=None, city_aug=0):
+
+    if dataset == 'voc':
+        transform_test = Compose(
+            [ToTensor(),
+             ZeroPad(size=input_sizes),
+             Normalize(mean=mean, std=std)])
+    elif dataset == 'city' or dataset == 'gtav' or dataset == 'synthia':  # All the same size
+        if city_aug == 2:  # ERFNet and ENet
+            transform_test = Compose(
+                [ToTensor(),
+                 Resize(size_image=input_sizes, size_label=input_sizes),
+                 LabelMap(test_label_id_map)])
+        elif city_aug == 1:  # City big
+            transform_test = Compose(
+                [ToTensor(),
+                 Resize(size_image=input_sizes, size_label=input_sizes),
+                 Normalize(mean=mean, std=std),
+                 LabelMap(test_label_id_map)])
+    else:
+        raise ValueError
+
+    # Not the actual test set (i.e. validation set)
+    test_set = StandardSegmentationDataset(root=test_base, image_set='val', transforms=transform_test,
+                                           data_set='city' if dataset == 'gtav' or dataset == 'synthia' else dataset)
+
+    val_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=1, num_workers=0,
+                                             shuffle=False)
+
+    # Testing
+    return val_loader
+
+
+def speed_evaluate_real(net, device, loader, num, count_interpolate=True):
+    net.eval()
+    iterable = iter(loader)
+
+    # Warm-up hardware
+    for _ in range(10):
+        image, _ = iterable.__next__()
+        image = image.to(device)
+        _ = net(image)['out']
+
+    # Timing with loading images from disk
+    gpu_time = 0
+    io_time = 0
+    with torch.no_grad():
+        for _ in tqdm(range(num)):
+            # I/O
+            torch.cuda.current_stream(device).synchronize()
+            temp = time.perf_counter()
+            image, _ = iterable.__next__()
+            image = image.to(device)
+            torch.cuda.current_stream(device).synchronize()
+            io_time += (time.perf_counter() - temp)
+
+            # GPU
+            torch.cuda.current_stream(device).synchronize()
+            temp = time.perf_counter()
+            output = net(image)['out']
+            if count_interpolate:
+                _ = torch.nn.functional.interpolate(output, size=image.shape[-2:], mode='bilinear', align_corners=True)
+            torch.cuda.current_stream(device).synchronize()
+            gpu_time += (time.perf_counter() - temp)
+
+    fps = num / (io_time + gpu_time)
+    gpu_fps = num / gpu_time
+
+    return fps, gpu_fps
+
+
+def speed_evaluate_simple(net, device, dummy, num, count_interpolate=True):
+    net.eval()
+    dummy = dummy.to(device)
+    output_size = dummy.shape[-2:]
+
+    # Warm-up hardware
+    for i in range(0, 10):
+        _ = net(dummy)['out']
+
+    # Timing
+    torch.cuda.current_stream(device).synchronize()
+    t_start = time.perf_counter()
+    with torch.no_grad():
+        for _ in tqdm(range(num)):
+            output = net(dummy)['out']
+            if count_interpolate:
+                _ = torch.nn.functional.interpolate(output, size=output_size, mode='bilinear', align_corners=True)
+    torch.cuda.current_stream(device).synchronize()
+    fps_gpu = num / (time.perf_counter() - t_start)
+
+    return fps_gpu
