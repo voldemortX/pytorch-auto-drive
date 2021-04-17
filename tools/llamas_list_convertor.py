a4fd11c95f36eb118413e4e76f489a991c7adb7e
cedricgsh <cedricgsh@sina.com>
2021-04-17 09:55:12 +0800

    add llamas utils

---
+++ tools/llamas_list_convertor.py
@@ -0,0 +1,15 @@
+from llamas_utils import generate_spline_annotation, generate_original_pixel_annotation
+
+if __name__ == '__main__':
+    #generate_spline_annotation()
+    generate_original_pixel_annotation()
+
+"""
+验证图像和label的匹配：
+            img_num = images_list[idx].split('/')[-1].split('_')[0:2]
+            json_num = json_list[idx].split('/')[-1].split('_')
+            json_num[1] = json_num[1].split('.')[0]
+
+            if img_num[0] != json_num[0] or img_num[1] != json_num[1]:
+                count += 1
+"""
