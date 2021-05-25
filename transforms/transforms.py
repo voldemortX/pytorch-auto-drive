67cd0d7f500766b48c772f86bcd18155a50259a8
voldemortX <zyfeng97@gmail.com>
2021-05-25 15:00:28 +0800

    border filter bug fix

--- transforms/transforms.py
+++ transforms/transforms.py
@@ -90,8 +90,8 @@ class Crop(object):
         # Essentially a translation with filtering, consider only crop area within the image
         ignore_filter = (points[:, :, 0] == ignore_x)
         points -= np.array([left, top], dtype=points.dtype)  # translation
-        ignore_by_crop = (points[:, :, 0] < 0) * (points[:, :, 0] > width) \
-            * (points[:, :, 1] < 0) * (points[:, :, 1] > height)  # filtering
+        ignore_by_crop = (points[:, :, 0] < 0) + (points[:, :, 0] > width) \
+            + (points[:, :, 1] < 0) + (points[:, :, 1] > height)  # filtering
         ignore_filter += ignore_by_crop
         points[:, :, 0] = points[:, :, 0] * ~ignore_filter + (-2) * ignore_filter
 
