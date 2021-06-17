12faacf28b0c3f2b6eada63f69c4a65946dba83c
voldemortX <zyfeng97@gmail.com>
2021-06-17 20:59:02 +0800

    RESA

--- torchvision_models/common_models.py
+++ torchvision_models/common_models.py
@@ -96,12 +96,48 @@ class SpatialConv(nn.Module):
 
 # REcurrent Feature-Shift Aggregator
 class RESA(nn.Module):
-    def __init__(self, num_channels=128):
+    def __init__(self, num_channels=128, iteration=5, alpha=2.0):
         super(RESA, self).__init__()
-        pass
+        # Different from SCNN, RESA uses bias=False & different convolution layers for each stride,
+        # i.e. 4 * iteration layers vs. 4 layers in SCNN, maybe special init is not needed anymore:
+        # https://github.com/ZJULearning/resa/blob/14b0fea6a1ab4f45d8f9f22fb110c1b3e53cf12e/models/resa.py#L21
+        self.iteration = iteration
+        self.alpha = alpha
+        self.conv_d = nn.ModuleList(nn.Conv2d(num_channels, num_channels, (1, 9), padding=(0, 4), bias=False)
+                                    for _ in range(iteration))
+        self.conv_u = nn.ModuleList(nn.Conv2d(num_channels, num_channels, (1, 9), padding=(0, 4), bias=False)
+                                    for _ in range(iteration))
+        self.conv_r = nn.ModuleList(nn.Conv2d(num_channels, num_channels, (9, 1), padding=(4, 0), bias=False)
+                                    for _ in range(iteration))
+        self.conv_l = nn.ModuleList(nn.Conv2d(num_channels, num_channels, (9, 1), padding=(4, 0), bias=False)
+                                    for _ in range(iteration))
 
     def forward(self, x):
-        pass
+        y = x
+        h, w = y.shape[-2:]
+        if 2 ** self.iteration > max(h, w):
+            print('Too many iterations for RESA, your image size may be too small.')
+
+        # We do indexing here to avoid extra input parameters at __init__(), with almost none computation overhead.
+        # Also, now it won't block arbitrary shaped input.
+        # Down
+        for i in range(self.iteration):
+            idx = (torch.arange(h) + h // 2 ** (self.iteration - i)) % h
+            y.add_(self.alpha * F.relu(self.conv_d[i](y[:, :, idx, :])))
+        # Up
+        for i in range(self.iteration):
+            idx = (torch.arange(h) - h // 2 ** (self.iteration - i)) % h
+            y.add_(self.alpha * F.relu(self.conv_u[i](y[:, :, idx, :])))
+        # Right
+        for i in range(self.iteration):
+            idx = (torch.arange(w) + w // 2 ** (self.iteration - i)) % w
+            y.add_(self.alpha * F.relu(self.conv_r[i](y[:, :, :, idx])))
+        # Left
+        for i in range(self.iteration):
+            idx = (torch.arange(w) - w // 2 ** (self.iteration - i)) % w
+            y.add_(self.alpha * F.relu(self.conv_l[i](y[:, :, :, idx])))
+
+        return y
 
 
 # Typical lane existence head originated from the SCNN paper
