61bf9fd1e4ba0fd280e25a3adc711b56d64003dd
voldemortX <zyfeng97@gmail.com>
2021-06-30 13:28:42 +0800

    full precision

--- tools/shells/resnet18s_lstr-aug_tusimple.sh
+++ tools/shells/resnet18s_lstr-aug_tusimple.sh
@@ -2,6 +2,6 @@
 # Training
 python main_landec.py --epochs=2000 --lr=0.00025 --batch-size=20 --dataset=tusimple --method=lstr --backbone=resnet18s --workers=16 --aug --exp-name=resnet18s_lstr-aug
 # Predicting lane points for testing
-python main_landec.py --state=2 --batch-size=80 --continue-from=resnet18s_lstr-aug.pt --dataset=tusimple --method=lstr --backbone=resnet18s --mixed-precision
+python main_landec.py --state=2 --batch-size=80 --continue-from=resnet18s_lstr-aug.pt --dataset=tusimple --method=lstr --backbone=resnet18s
 # Testing with official scripts
 ./autotest_tusimple.sh resnet18s_lstr-aug_tusimple test
