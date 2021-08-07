fd07a135a8ee4bc724af04b9f5cd82c9147e3cf7
voldemortX <zyfeng97@gmail.com>
2021-08-07 17:22:11 +0800

    stage3 resnet

--- torchvision_models/lane_detection/resa.py
+++ torchvision_models/lane_detection/resa.py
@@ -13,13 +13,13 @@ class RESANet(nn.Module):
         backbone = resnet.__dict__[backbone_name](
             pretrained=pretrained_backbone,
             replace_stride_with_dilation=[False, True, True])
-        return_layers = {'layer4': 'out'}
+        return_layers = {'layer3': 'out'}
         self.backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
-        in_channels = 2048 if backbone_name == 'resnet50' or backbone_name == 'resnet101' else 512
-        self.channel_reducer = RESAReducer(in_channels=in_channels, reduce=channel_reduce)
+        in_channels = 1024 if backbone_name == 'resnet50' or backbone_name == 'resnet101' else 256
+        self.channel_reducer = RESAReducer(in_channels=in_channels, reduce=channel_reduce)  #
         self.spatial_conv = RESA()
-        self.decoder = BUSD(num_classes=num_classes)
-        self.lane_classifier = EDLaneExist(num_output=num_classes - 1, flattened_size=flattened_size)
+        self.decoder = BUSD(num_classes=num_classes)  #
+        self.lane_classifier = EDLaneExist(num_output=num_classes - 1, flattened_size=flattened_size)  #
 
     def forward(self, x):
         x = self.backbone(x)['out']
