89d8240754f2071d4742d3dd549d9497bef674f0
voldemortX <zyfeng97@gmail.com>
2021-12-20 13:07:36 +0800

    shell

--- tools/shells/erfnet_baseline_tusimple.sh
+++ tools/shells/erfnet_baseline_tusimple.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Trained weights: erfnet_baseline_tusimple_20210424.pt
 # Training
-python main_landec.py --epochs=50 --lr=0.2 --batch-size=20 --dataset=tusimple --method=baseline --backbone=erfnet --mixed-precision --exp-name=erfnet_baseline_tusimple
+python main_landec.py --train --config=configs/lane_detection/baseline/erfnet_tusimple.py --mixed-precision
 # Predicting lane points for testing
-python main_landec.py --state=2 --batch-size=80 --continue-from=erfnet_baseline_tusimple.pt --dataset=tusimple --method=baseline --backbone=erfnet --mixed-precision --exp-name=erfnet_baseline_tusimple
+python main_landec.py --test --config=configs/lane_detection/baseline/erfnet_tusimple.py --mixed-precision
 # Testing with official scripts
-./autotest_tusimple.sh erfnet_baseline_tusimple test
+./autotest_tusimple.sh erfnet_baseline_tusimple test checkpoints
