e1431eab35c50b62940cb24a711bab1717967651
cedricgsh <guoshaohua@sjtu.edu.cn>
2021-12-24 10:17:00 +0800

    add resnet18

---
+++ configs/lane_detection/scnn/resnet18_tusimple.py
@@ -0,0 +1,89 @@
+# Data pipeline
+from configs.lane_detection.common.datasets.tusimple_seg import dataset
+from configs.lane_detection.common.datasets.train_level0_360 import train_augmentation
+from configs.lane_detection.common.datasets.test_360 import test_augmentation
+
+# Optimization pipeline
+from configs.lane_detection.common.optims.segloss_7class import loss
+from configs.lane_detection.common.optims.sgd02 import optimizer
+from configs.lane_detection.common.optims.ep50_poly_warmup200 import lr_scheduler
+
+# Default args that can be overridden in commandline
+train_args_default = dict(
+    exp_name='resnet18_scnn_tusimple',
+    workers=10,
+    batch_size=20,
+    checkpoint=None,
+    # Device args
+    world_size=0,
+    dist_url='env://',
+    device='cuda',
+
+    val_num_steps=0,  # Seg IoU validation (mostly useless)
+    save_dir='./checkpoints'
+)
+test_args_default = dict(
+    exp_name='resnet18_scnn_tusimple',
+    workers=4,
+    batch_size=80,
+    checkpoint='./checkpoints/resnet18_scnn_tusimple/model.pt',
+    # Device args
+    device='cuda',
+
+    save_dir='./checkpoints'
+)
+
+# Configs
+train = dict(
+    input_size=(360, 640),
+    original_size=(720, 1280),
+    num_classes=7,
+    num_epochs=50,
+    collate_fn=None,  # 'dict_collate_fn' for LSTR
+    seg=True  # Seg-based method or not
+)
+train.update(train_args_default)
+
+test = dict(
+    seg=True,
+    gap=10,
+    ppl=56,
+    thresh=0.3,
+    collate_fn=None,  # 'dict_collate_fn' for LSTR
+    input_size=(360, 640),
+    original_size=(720, 1280),
+    max_lane=5,
+    dataset_name='tusimple'
+)
+test.update(test_args_default)
+
+model = dict(
+    name='standard_segmentation_model',
+    backbone_cfg=dict(
+        name='predefined_resnet_backbone',
+        backbone_name='resnet18',
+        return_layer='layer4',
+        pretrained=True,
+        replace_stride_with_dilation=[False, True, True]
+    ),
+    reducer_cfg=dict(
+        name='RESAReducer',
+        in_channels=512,
+        reduce=128
+    ),
+    spatial_conv_cfg=dict(
+        name='SpatialConv',
+        num_channels=128
+    ),
+    classifier_cfg=dict(
+        name='DeepLabV1Head',
+        in_channels=128,
+        num_classes=7,
+        dilation=1
+    ),
+    lane_classifier_cfg=dict(
+        name='SimpleLaneExist',
+        num_output=7 - 1,
+        flattened_size=6160
+    )
+)
