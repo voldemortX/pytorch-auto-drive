46e4b5f1642c67a8afb6e8dcc843e332bf818068
cedricgsh <guoshaohua@sjtu.edu.cn>
2022-01-06 20:11:05 +0800

    ddp swin-tiny

--- configs/lane_detection/baseline/swin-tiny_culane.py
+++ configs/lane_detection/baseline/swin-tiny_culane.py
@@ -10,7 +10,7 @@ from configs.lane_detection.common.optims.adamw00006_swin import optimizer
 
 lr_scheduler = dict(
     name='poly_scheduler_with_warmup',
-    epochs=10,
+    epochs=36,
     power=1,  # ? Kept for consistency with official repo
     warmup_steps=1500,
     start_lr_ratio=1e-6,
@@ -19,12 +19,12 @@ lr_scheduler = dict(
 # Default args that can be overridden in commandline
 train_args_default = dict(
     exp_name='swin-tiny_baseline_culane',
-    workers=5,
-    batch_size=10,
+    workers=4,
+    batch_size=8,
     checkpoint=None,
     # Device args
-    world_size=0,
-    dist_url='env://',
+    world_size=2,
+    dist_url='tcp://localhost:12345',
     device='cuda',
     val_num_steps=0,  # Seg IoU validation (mostly useless)
     save_dir='./checkpoints'
@@ -44,7 +44,7 @@ train = dict(
     input_size=(288, 800),
     original_size=(590, 1640),
     num_classes=5,
-    num_epochs=12,
+    num_epochs=36,
     collate_fn=None,  # 'dict_collate_fn' for LSTR
     seg=True  # Seg-based method or not
 )
