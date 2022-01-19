32f6dc169d8c1d6c9587f0da4091f9f50d33dc3b
Zhengyang Feng <zyfeng97@outlook.com>
2022-01-19 12:35:58 +0800

    Update swin-tiny_baseline_culane.sh

--- tools/shells/swin-tiny_baseline_culane.sh
+++ tools/shells/swin-tiny_baseline_culane.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Trained weights: swin-tiny_baseline_culane_20220119.pt
 # Training
-python -m torch.distributed.launch --nproc_per_node=4 --use_env main_landec.py --train --config=configs/lane_detection/baseline/swin-tiny_culane.py
+python -m torch.distributed.launch --nproc_per_node=2 --use_env main_landec.py --train --config=configs/lane_detection/baseline/swin-tiny_culane.py
 # Predicting lane points for testing
 python main_landec.py --test --config=configs/lane_detection/baseline/swin-tiny_culane.py
 # Testing with official scripts
